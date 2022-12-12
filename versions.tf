eb16ea776613d7663321f625f712bea4a7c48f5c
“Inesh-Reddy” <ineshreddy007@gmail.com>
2022-12-12 11:39:49 +0530

    changing the version to 4.0

--- versions.tf
+++ versions.tf
@@ -4,7 +4,8 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.72"
+      version = ">= 4.0"
+      /* version = ">= 3.72" */
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
